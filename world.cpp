ada1a6159897343f3f5e3dab3e0ed31a7742559b
jsamp701 <constant8change6@gmail.com>
2013-02-28 12:43:30 -0500

    temp commit 2/28/13

--- world.cpp
+++ world.cpp
@@ -3,7 +3,7 @@
 //cmdString is a formatted string containing information regarding the number of players, names of the players, player class choices, and who is on which team
 World::World(string cmdString)
 {
-
+//needs coding
 }
 
 
@@ -21,12 +21,12 @@ void World::plCom(int ID, string com)
 
 Entity *World::getNAE(int locX, int locY, string origColor)
 {
-
+//needs coding
 }
 
 bool World::boundsCheck(int locX, int locY, int uSize)
 {
-
+//needs coding
 }
 
 void World::onTick()
@@ -43,10 +43,24 @@ void World::onTick()
 
 string World::save()
 {
-
+    stringstream save;
+    for(int i = 0; i < allEntities.size(); i++)
+    {
+        strm << allEntities.at(i)->save();
+    }
 }
 
 void World::load(string loadString)
 {
+//Needs coding
+}
 
+string World::Display()
+{
+    stringstream strm;
+    for(int i = 0; i < allEntities.size(); i++)
+    {
+        strm <<  allEntities.at(i)->Display();
+    }
+    return strm.str();
 }
