a6d06bdfd454ed65c3cd24ba92342d2dab88ff20
jmiller <jermiefb@gmail.com>
2013-03-09 15:33:46 -0500

    updated images

--- Client/entitylabel.h
+++ Client/entitylabel.h
@@ -15,14 +15,14 @@ protected:
     QString playerName;
 
 public:
-    EntityLabel(int id, int type, int posX, int posY, int pHealth, int nState. QString pName, QWidget *parent = 0);
+    EntityLabel(int id, int type, int posX, int posY, int pHealth, int nState, QString pName, QWidget *parent = 0);
     void mousePressEvent(QMouseEvent *ev);
     void setID(int newID);
     void setCounter(int newCounter){ counter = counter + newCounter;}
     int getID(){return ID;}
     int getCounter(){return counter;}
     void setHealth(int pHealth){percentHealth = pHealth;}
-    void set
+    //void set
 
 };
 
