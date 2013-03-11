ce021e47f0a2fb3ee117597e5b535df50f2eef85
jsamp701 <constant8change6@gmail.com>
2013-03-11 17:26:46 -0400

    hope this works

--- JoelClient/Client-build-desktop-Qt_4_8_1_in_PATH__System__Debug/moc_widget.cpp
+++ JoelClient/Client-build-desktop-Qt_4_8_1_in_PATH__System__Debug/moc_widget.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'widget.h'
 **
-** Created: Mon Mar 11 10:44:54 2013
+** Created: Mon Mar 11 17:24:55 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
 **
 ** WARNING! All changes made in this file will be lost!
